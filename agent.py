77456afcf479673a6cdce3dcde0aa39a7377b7e6
Mike Howles <mike.howles@gmail.com>
2022-06-02 22:42:48 +0000

    fix2

--- agent.py
+++ agent.py
@@ -121,24 +121,24 @@ def loop(args=None):
                 if not shape:
                     shape = "landscape"
 
-                cut_overview = "[12]*400+[4]*600"
-                cut_innercut = "[4]*400+[12]*600"
+                cut_overview = "'[12]*400+[4]*600'"
+                cut_innercut = "'[4]*400+[12]*600'"
 
                 if cut_schedule == "default":
-                    cut_overview = "[12]*400+[4]*600"
-                    cut_innercut = "[4]*400+[12]*600"
+                    cut_overview = "'[12]*400+[4]*600'"
+                    cut_innercut = "'[4]*400+[12]*600'"
 
                 if cut_schedule == "detailed-a":
-                    cut_overview = "[10]200+[8]200+[6]200+[2]200+[2]200"
-                    cut_innercut = "[0]200+[2]200+[6]200+[8]200+[10]200"
+                    cut_overview = "'[10]200+[8]200+[6]200+[2]200+[2]200'"
+                    cut_innercut = "'[0]200+[2]200+[6]200+[8]200+[10]200'"
 
                 if cut_schedule == "detailed-b":
-                    cut_overview = "[10]200+[8]200+[6]200+[4]200+[2]200"
-                    cut_innercut = "[2]200+[2]200+[8]200+[8]200+[10]200"
+                    cut_overview = "'[10]200+[8]200+[6]200+[4]200+[2]200'"
+                    cut_innercut = "'[2]200+[2]200+[8]200+[8]200+[10]200'"
 
                 if cut_schedule == "ram_efficient":
-                    cut_overview = "[10]200+[8]200+[5]200+[2]200+[2]200"
-                    cut_innercut = "[0]200+[2]200+[5]200+[7]200+[9]200"
+                    cut_overview = "'['10]200+[8]200+[5]200+[2]200+[2]200'"
+                    cut_innercut = "'[0]200+[2]200+[5]200+[7]200+[9]200'"
 
                 if render_type == "sketch":
                     if shape == "landscape":
@@ -191,9 +191,9 @@ def loop(args=None):
                     cmd.append(str(symmetry_loss_scale))
                 if cut_schedule != "default":
                     cmd.append("--cut_overview")
-                    cmd.append("[10]200+[8]200+[6]200+[4]200+[2]200")
+                    cmd.append(cut_overview)
                     cmd.append("--cut_innercut")
-                    cmd.append("[2]200+[2]200+[8]200+[8]200+[10]200")
+                    cmd.append(cut_innercut)
                 print(cmd)
                 logger.info(f"Running...:\n{job}")
                 s = time.time()
