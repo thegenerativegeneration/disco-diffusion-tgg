9ff9cc3b6c95e2f6118a9c5b62bd84b0dd4e2ffa
Mike Howles <mike.howles@gmail.com>
2022-07-01 22:01:25 +0000

    fix

--- docker/build-main.sh
+++ docker/build-main.sh
@@ -1,5 +1,5 @@
 docker build -t entmike/disco-diffusion-1 \
-    --build-arg model_path=models \
+    --build-arg model_path=/workspace/disco-diffusion-1/models \
     --build-arg base_image=pytorch/pytorch \
     --build-arg DD_VERSION=2.5 \
     -f Dockerfile .
